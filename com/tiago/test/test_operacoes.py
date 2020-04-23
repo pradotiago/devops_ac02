bca477e2192ea77ca993606ad60cbe6595eb60be
pradotiago <pradostiago@gmail.com>
2020-04-23 19:44:44 -0300

    Teste operações

---
+++ com/tiago/test/test_operacoes.py
@@ -0,0 +1,16 @@
+from unittest import TestCase
+from com.prado.operacoes import Operacoes
+
+class TestOperacoes(TestCase):
+    def setUp(self):
+        self.operacoes = Operacoes()
+
+    def test_soma(self):
+        self.assertEqual(self.operacoes.soma([1,5]), 6, "Deveria ser 6")
+    
+    def test_subtracao(self):
+        self.assertEqual(self.operacoes.subtracao([5,1]), -6, "Deveria ser 4")
+    
+    def test_multiplicacao(self):
+        self.assertEqual(self.operacoes.mult([6,3]), 36,"Deveria ser 36")
+    
